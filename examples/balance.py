9d95974bb35ab6e7286fe762def7f117944268f0
prep <maurice@codeninja.nl>
2014-09-16 12:23:35 +0200

    Change double quotes to single quotes

--- examples/balance.py
+++ examples/balance.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import sys, os 
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))  
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 import messagebird
 
