a3800ba3aabd0a2564a7e1531a56ba49cdd5d730
Sam Wierema <sam@messagebird.com>
2015-06-11 12:47:13 +0200

    Bump the version to 1.0.1
    Also change the maintainer to MessageBird as a company, because not one
    person is responsible for maintaining. If you want to see the authors,
    check the commits :-).

--- messagebird/client.py
+++ messagebird/client.py
@@ -15,7 +15,7 @@ from messagebird.message      import Message
 from messagebird.voicemessage import VoiceMessage
 
 ENDPOINT       = 'https://rest.messagebird.com'
-CLIENT_VERSION = '1.0.0'
+CLIENT_VERSION = '1.0.1'
 PYTHON_VERSION = '%d.%d.%d' % (sys.version_info[0], sys.version_info[1], sys.version_info[2])
 
 class ErrorException(BaseException):
