1d129a8d1a358c9b12fcdeb9b9e6318d98d02c01
Sam Wierema <sam@messagebird.com>
2016-03-30 11:01:41 +0200

    Add examples for each Verify API call

---
+++ examples/verify.py
@@ -0,0 +1,45 @@
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import messagebird
+
+# ACCESS_KEY = ''
+# VERIFY_ID  = ''
+
+try:
+  ACCESS_KEY
+except NameError:
+  print('You need to set an ACCESS_KEY constant in this file')
+  sys.exit(1)
+
+try:
+  VERIFY_ID
+except NameError:
+  print('You need to set a VERIFY_ID constant in this file')
+  sys.exit(1)
+
+try:
+  # Create a MessageBird client with the specified ACCESS_KEY.
+  client = messagebird.Client(ACCESS_KEY)
+
+  # Create a new Lookup HLR object.
+  verify = client.verify(VERIFY_ID)
+
+  # Print the object information.
+  print('\nThe following information was returned as a Verify object:\n')
+  print('  id                  : %s' % verify.id)
+  print('  href                : %s' % verify.href)
+  print('  recipient           : %s' % verify.recipient)
+  print('  reference           : %s' % verify.reference)
+  print('  messages            : %s' % verify.messages)
+  print('  status              : %s' % verify.status)
+  print('  createdDatetime     : %s' % verify.createdDatetime)
+  print('  validUntilDatetime  : %s\n' % verify.validUntilDatetime)
+
+except messagebird.client.ErrorException as e:
+  print('\nAn error occured while requesting a Verify object:\n')
+
+  for error in e.errors:
+    print('  code        : %d' % error.code)
+    print('  description : %s' % error.description)
+    print('  parameter   : %s\n' % error.parameter)
