271f377079f220c9454c8b0fa390fde940e477fe
Sam Wierema <sam@messagebird.com>
2016-03-30 10:43:30 +0200

    Only return those variables in the Verify object that are returned from the API

--- messagebird/verify.py
+++ messagebird/verify.py
@@ -1,24 +1,15 @@
-from messagebird.base      import Base
-from messagebird.recipient import Recipient
-from messagebird.message import Message
+from messagebird.base import Base
 
 class Verify(Base):
   def __init__(self):
-    self.id                 = None
-    self.href               = None
-    self.type               = None 
-    self.originator         = None 
-    self.reference          = None 
-    self.template           = None
-    self.timeout            = None
-    self.tokenLength        = None
-    self.voice              = None
-    self.language           = None
-    self.status             = None
-    self.recipient          = None    
-    self._createdDatetime   = None
-    self._validUntilDatetime= None
-    self._messages          = None
+    self.id                  = None
+    self.href                = None
+    self.recipient           = None
+    self.reference           = None
+    self.messages            = None
+    self.status              = None
+    self._createdDatetime    = None
+    self._validUntilDatetime = None
 
 
   @property
@@ -36,12 +27,4 @@ class Verify(Base):
 
   @validUntilDatetime.setter
   def validUntilDatetime(self, value):
-    self._validUntilDatetime = self.value_to_time(value)    
-
-  @property
-  def messages(self):
-    return self._messages
-
-  @messages.setter
-  def messages(self, value):
-    self._messages = Message().load(value) 
+    self._validUntilDatetime = self.value_to_time(value)
