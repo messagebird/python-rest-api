58b18e6c7b94492d02e89cc939353773c9e67fb4
Oleh Bahinskyi <oleh@messagebird.com>
2020-01-13 11:45:22 +0100

    pep8

--- examples/number_available_list.py
+++ examples/number_available_list.py
@@ -14,38 +14,38 @@ parser.add_argument('--accessKey', help='access key for MessageBird API', type=s
 args = vars(parser.parse_args())
 
 try:
-  # Create a MessageBird client with the specified accessKey.
-  client = messagebird.Client(args['accessKey'])
+    # Create a MessageBird client with the specified accessKey.
+    client = messagebird.Client(args['accessKey'])
 
-  # Fetch the NumberList object with specified params, limit, offset.
-  params = {'features': ['sms', 'voice'], 'number': 319}
-  numbers = client.available_numbers_list('NL', params, 2, 0)
+    # Fetch the NumberList object with specified params, limit, offset.
+    params = {'features': ['sms', 'voice'], 'number': 319}
+    numbers = client.available_numbers_list('NL', params, 2, 0)
 
-  # Print the object information.
-  print('\nThe following information was returned as a %s object:\n' % numbers.__class__)
-  if numbers.items is not None:
-    print('  Containing the the following items:')
-    for item in numbers.items:
-      print('  {')
-      print('    number                : %s' % item.number)
-      print('    country               : %s' % item.country)
-      print('    region                : %s' % item.region)
-      print('    locality              : %s' % item.locality)
-      print('    features              : %s' % item.features)
-      print('    tags                  : %s' % item.tags)
-      print('    type                  : %s' % item.type)
-      print('    status                : %s' % item.status)
-      print('  },')
-  else:
-    print('  With an empty response.')
+    # Print the object information.
+    print('\nThe following information was returned as a %s object:\n' % numbers.__class__)
+    if numbers.items is not None:
+        print('  Containing the the following items:')
+        for item in numbers.items:
+            print('  {')
+            print('    number                : %s' % item.number)
+            print('    country               : %s' % item.country)
+            print('    region                : %s' % item.region)
+            print('    locality              : %s' % item.locality)
+            print('    features              : %s' % item.features)
+            print('    tags                  : %s' % item.tags)
+            print('    type                  : %s' % item.type)
+            print('    status                : %s' % item.status)
+            print('  },')
+    else:
+        print('  With an empty response.')
 
 except messagebird.client.ErrorException as e:
-  print('\nAn error occured while requesting a NumberList object:\n')
+    print('\nAn error occured while requesting a NumberList object:\n')
 
-  for error in e.errors:
-    print('  code        : %d' % error.code)
-    print('  description : %s' % error.description)
-    print('  parameter   : %s\n' % error.parameter)
+    for error in e.errors:
+        print('  code        : %d' % error.code)
+        print('  description : %s' % error.description)
+        print('  parameter   : %s\n' % error.parameter)
 
 except requests.exceptions.HTTPError as e:
     print('\nAn HTTP exception occurred while fetching all purchased phone numbers:')
