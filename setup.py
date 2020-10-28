40bbe797fb9da62bcdedfad4dfabe5cefc288908
Mehmet M. Inanc <max.inanc@messagebird.com>
2020-10-28 11:23:31 +0100

    * Bumps version to 2.0.0. * Moves requirements to `setup.py`. * Removes python2 from `setup.py`. * Removes python3.5 from `setup.py`. * Adds new major versions of python3.

--- setup.py
+++ setup.py
@@ -2,31 +2,38 @@ from os import path
 from setuptools import setup
 from io import open
 
-def get_description():
-    working_directory = path.abspath(path.dirname(__file__))
-    readme_path = path.join(working_directory, 'README.md')
-    with open(readme_path, encoding='utf-8') as f:
-        return (f.read(), 'text/markdown')
-
-description, description_content_type = get_description()
+with open('README.md', encoding='utf-8') as f:
+    description = f.read()
 
 setup(
     name                          = 'messagebird',
     packages                      = ['messagebird'],
-    version                       = '1.5.0',
+    version                       = '2.0.0',
     description                   = "MessageBird's REST API",
     author                        = 'MessageBird',
     author_email                  = 'support@messagebird.com',
     long_description              = description,
-    long_description_content_type = description_content_type,
+    long_description_content_type = 'text/markdown',
     url                           = 'https://github.com/messagebird/python-rest-api',
-    download_url                  = 'https://github.com/messagebird/python-rest-api/tarball/1.4.1',
+    download_url                  = 'https://github.com/messagebird/python-rest-api/tarball/2.0.0',
     keywords                      = ['messagebird', 'sms'],
     install_requires              = ['requests>=2.4.1', 'python-dateutil>=2.6.0'],
+    extras_require               = {
+        'dev': [
+            'pytest',
+            'pytest-cov',
+            'mock>=2.0',
+            'codecov',
+            'pycodestyle',
+        ]
+    },
     license                       = 'BSD-2-Clause',
     classifiers                   = [
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: OS Independent',
     ],
 )
