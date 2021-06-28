50a2e1803a8013c46b3ac27ca13a5ff54da4bd1a
Mehmet M. Inanc <max.inanc@messagebird.com>
2021-06-28 16:00:25 +0200

    * Gets rid of formatting issues in tests. * Triple quotes json test fixtures for better readability. * Removes ignores from pycodestyle.

--- messagebird/serde.py
+++ messagebird/serde.py
@@ -1,15 +1,8 @@
 import json
-import sys
 
 
 def json_serialize(obj):
-    if sys.version_info > (3, 0):
-        try:
-            return json.dumps(obj, ensure_ascii=False).encode('utf-8')
-        except TypeError:
-            return json.dumps(obj, default=lambda o: o.__dict__, ensure_ascii=False).encode('utf-8')
-    else:
-        try:
-            return json.dumps(obj, ensure_ascii=False, encoding='utf-8')
-        except TypeError:
-            return json.dumps(obj, default=lambda o: o.__dict__, ensure_ascii=False, encoding='utf-8')
+    try:
+        return json.dumps(obj, ensure_ascii=False).encode('utf-8')
+    except TypeError:
+        return json.dumps(obj, default=lambda o: o.__dict__, ensure_ascii=False).encode('utf-8')
--- tests/test_serde.py
+++ tests/test_serde.py
@@ -1,5 +1,4 @@
 # coding=utf-8
-import sys
 import unittest
 
 from messagebird.serde import json_serialize
@@ -7,17 +6,10 @@ from messagebird.serde import json_serialize
 
 class TestJSONSerDe(unittest.TestCase):
 
-    @staticmethod
-    def encode_if_py3(s):
-        if sys.version_info > (3, 0):
-            return s.encode('utf-8')
-        else:
-            return s
-
     def test_tr_check(self):
         self.assertEqual(json_serialize({'body': 'Pijamalı hasta, yağız şoföre çabucak güvendi.'}),
-                         self.encode_if_py3("""{"body": "Pijamalı hasta, yağız şoföre çabucak güvendi."}"""))
+                         """{"body": "Pijamalı hasta, yağız şoföre çabucak güvendi."}""".encode("utf-8"))
 
     def test_jp_check(self):
         self.assertEqual(json_serialize({'body': 'いろはにほへとちりぬるを'}),
-                         self.encode_if_py3("""{"body": "いろはにほへとちりぬるを"}"""))
+                         """{"body": "いろはにほへとちりぬるを"}""".encode("utf-8"))
