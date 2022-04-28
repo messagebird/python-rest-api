9c8476cbb1b22285f4edaf6f61ba00bcce1501a6
Khanh Nguyen <khanh@nqkdev.com>
2022-04-28 11:35:03 +0200

    Add python release workflow and release JWT webhook signature (#81)
    * Add python release workflow
    
    * Fix changelog preparation release step
    
    * Use gh to checkout PR
    
    * Fixed CHANGELOG.md and added pull request template
    
    * Removed download_url
    
    Co-authored-by: khanh.nguyen <khanh.nguyen@messagebird.com>

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
-from os import path
+import os
+import re
 from setuptools import setup
 from io import open
+
+with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'messagebird/version.py'), 'r') as fd:
+    VERSION = re.search(r'^VERSION = [\']([^\']*)[\']',
+                        fd.read(), re.MULTILINE).group(1)
+
+if not VERSION:
+    raise RuntimeError('Ensure `VERSION` is correctly set in ./messagebird/version.py')
 
 with open('README.md', encoding='utf-8') as f:
     description = f.read()
@@ -8,14 +16,13 @@ with open('README.md', encoding='utf-8') as f:
 setup(
     name                          = 'messagebird',
     packages                      = ['messagebird'],
-    version                       = '2.0.0',
+    version                       = VERSION,
     description                   = "MessageBird's REST API",
     author                        = 'MessageBird',
     author_email                  = 'support@messagebird.com',
     long_description              = description,
     long_description_content_type = 'text/markdown',
     url                           = 'https://github.com/messagebird/python-rest-api',
-    download_url                  = 'https://github.com/messagebird/python-rest-api/tarball/2.0.0',
     keywords                      = ['messagebird', 'sms'],
     install_requires              = ['requests>=2.4.1', 'python-dateutil>=2.6.0', 'pyjwt>=2.1.0'],
     extras_require               = {
